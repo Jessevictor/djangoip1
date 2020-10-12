2628a4f72bc533b95a6ed4aaf199df85113c56df
jessevictor <kongoijesse97@gmail.com>
2020-10-12 15:11:59 +0300

    made migrations

--- gallery/settings.py
+++ gallery/settings.py
@@ -123,8 +123,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'gallery/static')
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
