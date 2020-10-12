9e157984c1b17f8217b55a359ad96008c3fd9551
jessevictor <kongoijesse97@gmail.com>
2020-10-12 19:18:27 +0300

    add copy fuctionality

--- Photos/static/js/index.js
+++ Photos/static/js/index.js
@@ -1,14 +1,23 @@
-$(function() {
+$(function () {
     var selectedClass = "";
-    $(".filter").click(function(){
-    selectedClass = $(this).attr("data-rel");
-    $("#gallery").fadeTo(100, 0.1);
-    $("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
-    setTimeout(function() {
-    $("."+selectedClass).fadeIn().addClass('animation');
-    $("#gallery").fadeTo(300, 1);
-    }, 300);
+    $(".filter").click(function () {
+        selectedClass = $(this).attr("data-rel");
+        $("#gallery").fadeTo(100, 0.1);
+        $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
+        setTimeout(function () {
+            $("." + selectedClass).fadeIn().addClass('animation');
+            $("#gallery").fadeTo(300, 1);
+        }, 300);
     });
-    });
+});
 
-    
+function myFunction() {
+ 
+    var copyText = document.getElementById("myInput");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+  
+    document.execCommand("copy");
+  
+    alert("Copied the text: " + copyText.value);
+  }
